b73a8ed94cb3a9f19aad07fa0953678ef87c092f
bibs28 <nabibmalik@gmail.com>
2025-04-21 19:19:35 +0100

    Var

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,3 +1,14 @@
 terraform {
-  required_version = ">= 1.3"
+  required_providers {
+    panos = {
+      source  = "PaloAltoNetworks/panos"
+      version = "~> 1.10"
+    }
+  }
+}
+
+provider "panos" {
+  hostname = "192.168.0.156"          
+  api_key  = var.LUFRPT1vRFdGN1BLU2EwYmpUSnRraXNxWDkwT0FNOFU9ZytqWjRUUSt4bnhsbVY2VEtGbTIvTGRiR0pGMHozUTdCbGVzaWlPVjBxbmtwTEU2Y0NuU2ZTZlVDU1RybmNrWg     
+  timeout  = 60
 }
---
+++ variables.tf
@@ -0,0 +1,4 @@
+variable "panos_api_key" {
+  description = "Palo Alto API key"
+  type        = string
+  sensitive   = true 
