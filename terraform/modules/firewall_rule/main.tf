82b2dd0f5e21c16d20cfbb29fe570f7106b2d664
Starkiller283 <deathstar283@outlook.com>
2025-10-01 15:56:47 +0100

    fix: accept string port from CSV sourcefix: accept string port from CSV sourceUpdate main.tf

--- terraform/modules/firewall_rule/main.tf
+++ terraform/modules/firewall_rule/main.tf
@@ -5,22 +5,19 @@ terraform {
     }
   }
 }
-
 variable "rules" {
   type = list(object({
     rule_name       = string
     source_ip       = string
     destination_ip  = string
-    port            = number
+    port            = string
     protocol        = string
     action          = string
     description     = string
   }))
 }
-
 resource "panos_security_policy" "firewall_rule" {
   vsys = "vsys1"
-
   dynamic "rule" {
     for_each = var.rules
     content {
@@ -30,7 +27,7 @@ resource "panos_security_policy" "firewall_rule" {
       source_addresses      = [rule.value.source_ip]
       destination_addresses = [rule.value.destination_ip]
       applications          = ["any"]
-      services              = ["service-" + rule.value.protocol + "-" + tostring(rule.value.port)]
+      services              = ["service-" + rule.value.protocol + "-" + rule.value.port]
       action                = rule.value.action
       description           = rule.value.description
       source_users          = ["any"]
