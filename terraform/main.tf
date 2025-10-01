33b730b2790fc6f2994cf755d10d3d97fbf9d969
Starkiller283 <deathstar283@outlook.com>
2025-10-01 16:00:04 +0100

    fix: cast port to number after CSV importUpdate main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,5 +1,17 @@
 locals {
-  firewall_rules = csvdecode(file("${path.module}/../rules.csv"))
+  csv_data = csvdecode(file("${path.module}/../rules.csv"))
+  
+  firewall_rules = [
+    for rule in local.csv_data : {
+      rule_name      = rule.rule_name
+      source_ip      = rule.source_ip
+      destination_ip = rule.destination_ip
+      port           = tonumber(rule.port)
+      protocol       = rule.protocol
+      action         = rule.action
+      description    = rule.description
+    }
+  ]
 }
 
 module "firewall_rules" {
